d212c39c5af2da8dc96ac1e93475a9991e89db38
arif109 <mdazizularif109@gmail.com>
2024-12-15 14:08:23 +0600

    added new mega menu

---
+++ mega-menu.php
@@ -0,0 +1,178 @@
+<div class="container">
+    <div class="panel-panel panel-col-bottom">
+        <div class="inside">
+            <div class="contextual-links-region panel-pane pane-block pane-tb-megamenu-main-menu">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-sx-12">
+                        <nav id="main-nav">
+                            <ul id="main-menu" class="clearfix sf-js-enabled sf-arrows">
+                                <li class=""> <a href="#" class="sf-with-ul"> About Us </a>
+                                    <ul style="display: none;">
+                                        <li class=""> <a href="#" class="sf-with-ul"> The University </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh/about-the-campus"> Overview </a></li>
+                                                <li> <a href="#" class="sf-with-ul"> Awards &amp; Recognition </a>
+                                                    <ul style="display: none;">
+                                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-ucsi/affiliation-ranking-recognition/ranking"> Ranking </a></li>
+                                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/https%3A//bangladesh.ucsiuniversity.edu.my/about-ucsi/affiliation-ranking-recognition/recognition"> Recognition </a></li>
+                                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-ucsi/affiliation-ranking-recognition/affiliation"> Affliliation </a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-ucsi/the-founder?display=tu"> The Founder </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/staff-directory/bangladesh/board-of-directors-and-top-management?display=ul"> Board of Directors </a></li>
+                                        <li class=""> <a href="#" class="sf-with-ul"> University Leadership </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/the-pro-chancellor?display=ul"> The Pro-Chancellor </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/the-vice-chancellor?display=ul"> The Vice-Chancellor </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/university-council"> University Council </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/senate-members"> Senate Members </a></li>
+                                            </ul>
+                                        </li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/academic-advisory-board"> Board of Academic Advisors </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/deputy-vice-chancellor"> Deputy Vice-Chancellor </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/provost"> Provost </a></li>
+                                        <li class=""> <a href="https://www.bangladesh.ucsiuniversity.edu.my/staff-directory-0"> Staff Directory </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/registers-office"> Registrar's Office </a></li>
+                                    </ul>
+                                </li>
+                                <li class=""> <a href="#" class="sf-with-ul"> Academic </a>
+                                    <ul style="display: none;">
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/programmes/bangladesh"> Programmes </a></li>
+                                        <li> <a href="#" class="sf-with-ul"> Faculties </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/school-of-graduate-studies"> School of Graduate Studies </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-business-and-management"> Faculty of Business And Management </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-engineering"> Faculty of Engineering </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-hospitality-and-tourism-management"> Faculty of Hospitality &amp; Tourism Management </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-social-sciences-and-liberal-arts"> Faculty of Social Sciences &amp; Liberal Arts </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-creative-arts-design-and-architecture"> Faculty of Creative Arts, Design And Architecture </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-computer-science-and-digital-innovation"> Faculty of Computer Science And Digital Innovation </a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li> <a href="#" class="sf-with-ul"> Admissions </a>
+                                    <ul style="display: none;">
+                                        <li> <a href="#" class="sf-with-ul"> Bangladeshi Students </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/admission/bangladeshi-students/overview"> Overview </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/admission/bangladeshi-students/admission-procedure"> Admission Procedure </a></li>
+                                            </ul>
+                                        </li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/intakes-and-calendar"> Intakes and Calendar </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/scholarships"> Scholarships </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/Tuition-Fees"> Tuition Fees </a></li>
+                                        <li> <a href="https://bangladesh.ucsiuniversity.edu.my/downloads"> Downloads </a></li>
+                                    </ul>
+                                </li>
+                                <li class=""> <a href="#" class="sf-with-ul"> Life at UCSI </a>
+                                    <ul style="display: none;">
+                                        <li> <a href="#" class="sf-with-ul"> Library Services </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/library-policy"> Library </a></li>
+                                                <li> <a href="https://koha.ucsiuniversity.edu.my/cgi-bin/koha/opac-main.pl"> OPAC </a></li>
+                                                <li> <a href="https://lib.ucsiuniversity.edu.my/e-resource/online-database"> Online Database </a></li>
+                                            </ul>
+                                        </li>
+                                        <li> <a href="#" class="sf-with-ul"> Student Experience </a>
+                                            <ul style="display: none;">
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/student-affairs-and-alumni"> Student Affairs and Alumni </a></li>
+                                                <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/students-activities-and-recreation-new"> Students' Activities and Recreation </a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class=""> <a href="#" class="sf-with-ul"> Facilities </a>
+                                    <ul style="display: none;">
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/all-campus-facilities"> Campus Facilities </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/student-counsiling"> Student Counselling and Enrollment Center </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/security-services"> Security Services </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/lobby"> Spacious Lobby </a></li>
+                                    </ul>
+                                </li>
+                                <li class=""> <a href="#" class="sf-with-ul"> Publicity </a>
+                                    <ul style="display: none;">
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/featured-in-news/all/all"> Press &amp; Publicity </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/photo-gallery/all/all"> Photo Gallery </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/video-gallery"> Video Gallery </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/announcement"> Notices &amp; Announcements </a></li>
+                                    </ul>
+                                </li>
+                                <li> <a href="https://www.ucsiuniversity.edu.my/"> UCSI University </a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    /* Hide all submenus by default */
+    .sf-with-ul {
+        cursor: pointer;
+    }
+
+    .sf-with-ul+ul {
+        display: none;
+        position: absolute;
+        top: 0;
+        left: 100%;
+        z-index: 1000;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+
+    /* Styling for the arrows */
+    .sf-arrows .sf-with-ul:after {
+        content: '\25BC';
+        /* Down arrow */
+        margin-left: 5px;
+    }
+
+    .sf-arrows .sf-with-ul.sf-hover:after {
+        content: '\25B2';
+        /* Up arrow */
+    }
+
+    /* Hover effect to show submenus */
+    .sf-arrows>li:hover>ul {
+        display: block;
+    }
+
+    /* Additional nested submenus should also follow this pattern */
+    .sf-arrows>li>ul>li>ul {
+        display: none;
+        left: 100%;
+        top: 0;
+    }
+
+    /* Display nested submenus when their parent list item is hovered */
+    .sf-arrows>li>ul>li:hover>ul {
+        display: block;
+    }
+</style>
+
+<script>
+    $(document).ready(function() {
+        // Handle hover on main menu items
+        $('#main-menu > li').hover(function() {
+            $(this).children('ul').stop(true, true).slideDown(300);
+            $(this).children('a').addClass('sf-hover');
+        }, function() {
+            $(this).children('ul').stop(true, true).slideUp(300);
+            $(this).children('a').removeClass('sf-hover');
+        });
+
+        // Handle hover for nested submenus
+        $('#main-menu li > ul li').hover(function() {
+            $(this).children('ul').stop(true, true).slideDown(300);
+        }, function() {
+            $(this).children('ul').stop(true, true).slideUp(300);
+        });
+    });
+</script>

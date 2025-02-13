2f32431cc4beb3727d95656538a4854df70bfef9
azizularif420 <arif@mazegeek.com>
2025-02-13 20:26:47 +0600

    library

---
+++ library-opening.php
@@ -0,0 +1,76 @@
+    <div class="col-md-12">
+          <div class="widget kopa-owl-5-widget">
+               <div class="widget-title widget-title-custom text-center">
+                    <span></span>
+                    <h2>Schedule Table</h2>
+               </div>
+           </div>
+        <p>UCSI University Bangladesh Branch Campus Library supports students and researchers through the power of information and learning. Collections include an extensive collection of eBooks (When the service is being introduced), print books and online resources.</p>
+        <table class="scholar">
+            <tbody>
+                <tr>
+                    <th>
+                        Day
+                    </th>
+                    <th>
+                        Time
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Saturday</p>
+                    </td>
+                    <td>
+                        <p>09:30 am - 07:30 pm</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Sunday - Monday</p>
+                    </td>
+                    <td>
+                        <p>09:30 am - 05:30 pm</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Tuesday - Thursday</p>
+                    </td>
+                    <td>
+                        <p>09:30 am - 05:30 pm</p>
+                    </td>
+                </tr>
+                <!-- <tr>
+                    <td>
+                        <p>Friday </p>
+                    </td>
+                    <td>
+                        <p>Weekend (CLOSED) - **Based on the needs and demand of the students may be kept open on Friday for few hours.</p>
+                    </td>
+                </tr> -->
+                <tr>
+                    <td>
+                        <p>Friday </p>
+                    </td>
+                    <td>
+                        <p>Weekly Holiday</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Government Declared Public Holiday</p>
+                    </td>
+                    <td>
+                        <p>CLOSED</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <p class="pt-20"><b>Please Note:</b></p>
+        <ul>
+            <li>Library users should leave the library at the second announcement before the closure of the library.</li>
+            <li>No transactions at the counter (Borrowing/returning and renewal) can be made 30 minutes before closing time.</li>
+            <li>Operating and closing Hours are subject to change by management without prior notice.</li>
+            <li>The library shall not be responsible for the safety of any personal property left within the library.</li>
+        </ul>
+    </div>

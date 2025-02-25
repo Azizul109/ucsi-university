d38aa6bd174e3ecb3df2c5ac4b0fa20240718bb5
Fahim6989 <fahimfoisoleee@gmail.com>
2025-02-25 14:23:28 +0600

    announcemnt and scholarship

--- scholarship.php
+++ scholarship.php
@@ -23,199 +23,199 @@
             </div>
         </div>
 
-        <h4 class="scholar">Scholarship offer For SSC/HSC &amp; Alim/Dakhil or Equivalent</h4>
+        <h4 class="scholar">Scholarship offer For SSC/HSC &amp; Dakhil/Alim or Equivalent</h4>
 
         <div style="overflow-x:auto;">
-        <table class="scholar">
-            <tbody>
-                <tr>
-                    <th>
-                        Result under General Board (Including fourth subject)
-                    </th>
-                    <th>
-                        Percentage of Scholarship
-                    </th>
-                    <th>
-                        CGPA to be obtained
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        <p>Golden GPA-5 both in SSC/Alim and in HSC/Dakhil or equivalent &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>100%</p>
-                    </td>
-                    <td>
-                        <p>3.50</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>GPA-5 both in SSC/Alim and in HSC/Dakhil or equivalent &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>75%</p>
-                    </td>
-                    <td>
-                        <p>3.25</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>Golden GPA-5 in HSC/Dakhil or equivalent &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>50%</p>
-                    </td>
-                    <td>
-                        <p>3.25</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>GPA of 2.5 and above in both SSC/Alim and in HSC/Dakhil or equivalent &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>20%</p>
-                    </td>
-                    <td>
-                        <p>3.00</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            <table class="scholar">
+                <tbody>
+                    <tr>
+                        <th>
+                            Result under General Board (Including fourth subject)
+                        </th>
+                        <th>
+                            Percentage of Scholarship
+                        </th>
+                        <th>
+                            CGPA to be obtained
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Golden GPA-5 both in SSC/Dakhil and in HSC/Alim or equivalent &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>100%</p>
+                        </td>
+                        <td>
+                            <p>3.50</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>GPA-5 both in SSC/Dakhil and in HSC/Alim or equivalent &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>75%</p>
+                        </td>
+                        <td>
+                            <p>3.25</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Golden GPA-5 in HSC/Alim or equivalent &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>50%</p>
+                        </td>
+                        <td>
+                            <p>3.25</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>GPA of 2.5 and above in both SSC/Dakhil and in HSC/Alim or equivalent &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>20%</p>
+                        </td>
+                        <td>
+                            <p>3.00</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
         <h4 class="scholar pt-50">Scholarship offer for O level/A Level or Equivalent</h4>
 
         <div style="overflow-x:auto;">
-        <table class="scholar">
-            <tbody>
-                <tr>
-                    <th>
-                        Result of English Medium background
-                    </th>
-                    <th>
-                        Percentage of Scholarship
-                    </th>
-                    <th>
-                        CGPA to be obtained
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        <p>5 “A”s in “O” levels and 2 “A”s in “A” levels &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>100%</p>
-                    </td>
-                    <td>
-                        <p>3.50</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>3 “A”s in “O” levels and 1 “A” in “A” levels &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>75%</p>
-                    </td>
-                    <td>
-                        <p>3.25</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>2 “A”s “O” levels and 1 “A” in “A” levels &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>50%</p>
-                    </td>
-                    <td>
-                        <p>3.25</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>2 “B” “O” levels and 1 “B” in “A” levels &amp; placement test result.</p>
-                    </td>
-                    <td>
-                        <p>20%</p>
-                    </td>
-                    <td>
-                        <p>3.00</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            <table class="scholar">
+                <tbody>
+                    <tr>
+                        <th>
+                            Result of English Medium background
+                        </th>
+                        <th>
+                            Percentage of Scholarship
+                        </th>
+                        <th>
+                            CGPA to be obtained
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>5 “A”s in “O” levels and 2 “A”s in “A” levels &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>100%</p>
+                        </td>
+                        <td>
+                            <p>3.50</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>3 “A”s in “O” levels and 1 “A” in “A” levels &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>75%</p>
+                        </td>
+                        <td>
+                            <p>3.25</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2 “A”s “O” levels and 1 “A” in “A” levels &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>50%</p>
+                        </td>
+                        <td>
+                            <p>3.25</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2 “B” “O” levels and 1 “B” in “A” levels &amp; placement test result.</p>
+                        </td>
+                        <td>
+                            <p>20%</p>
+                        </td>
+                        <td>
+                            <p>3.00</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
         <div style="display: flex; flex-direction: column; align-items: center;">
 
-        <h4 class="scholar pt-50">IELTS/TOEFL Scholarship</h4>
-    
-        <div style="overflow-x:auto;">
-        <table class="scholar">
-            <tbody>
-                <tr>
-                    <th>
-                        IELTS Score
-                    </th>
-                    <th>
-                        TOEFL Score
-                    </th>
-                    <th>
-                        Percentage of scholarship
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        <p>8.0 and above & placement test result</p>
-                    </td>
-                    <td>
-                        <p>95-100</p>
-                    </td>
-                    <td>
-                        <p>100%</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>7.5 and above & placement test result</p>
-                    </td>
-                    <td>
-                        <p>90-94</p>
-                    </td>
-                    <td>
-                        <p>75%</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>7.0 and above & placement test result</p>
-                    </td>
-                    <td>
-                        <p>85-89</p>
-                    </td>
-                    <td>
-                        <p>50%</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>6.5 and above & placement test result</p>
-                    </td>
-                    <td>
-                        <p>80-84</p>
-                    </td>
-                    <td>
-                        <p>20%</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            <h4 class="scholar pt-50">IELTS/TOEFL Scholarship</h4>
+
+            <div style="overflow-x:auto;">
+                <table class="scholar">
+                    <tbody>
+                        <tr>
+                            <th>
+                                IELTS Score
+                            </th>
+                            <th>
+                                TOEFL Score
+                            </th>
+                            <th>
+                                Percentage of scholarship
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>8.0 and above & placement test result</p>
+                            </td>
+                            <td>
+                                <p>95-100</p>
+                            </td>
+                            <td>
+                                <p>100%</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>7.5 and above & placement test result</p>
+                            </td>
+                            <td>
+                                <p>90-94</p>
+                            </td>
+                            <td>
+                                <p>75%</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>7.0 and above & placement test result</p>
+                            </td>
+                            <td>
+                                <p>85-89</p>
+                            </td>
+                            <td>
+                                <p>50%</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>6.5 and above & placement test result</p>
+                            </td>
+                            <td>
+                                <p>80-84</p>
+                            </td>
+                            <td>
+                                <p>20%</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     </div>
 </div>

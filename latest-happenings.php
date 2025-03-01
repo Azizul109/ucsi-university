f47ed152719dfa2fa3512caac576e15a1a663c1e
Azizul109 <azizularifnahim52@gmail.com>
2025-03-01 17:58:17 +0600

    latest happenings section

---
+++ latest-happenings.php
@@ -0,0 +1,100 @@
+<section class="kopa-area kopa-area-5 kopa-area-parallax-2">
+    <div class="container">
+        <div class="row">
+            <div class="panel-display panel-1col clearfix" id="mini-panel-latest_happenings">
+                <div class="panel-panel panel-col">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="view view-front-page view-id-front_page view-display-id-latest_happenings view-dom-id-58bb3d962496b42be84b874c1acfc58d">
+                            <div class="widget kopa-nothumb-widget">
+                                <div class="widget-title widget-title-s7 clearfix"> <i class="fa fa-newspaper-o pull-left"></i>
+                                    <h2 class="pull-left"><span class="whiteFont">LATEST <br>HAPPENINGS</span></h2> <span class="pull-right white"><a href="https://www.bangladesh.ucsiuniversity.edu.my/photo-gallery/all/all" style="color: white;">View More</a></span>
+                                </div>
+                                <div class="widget-content">
+                                    <div class="row">
+                                        <div class="views-field views-field-title"> <span class="field-content">
+                                                <div class="col-md-4 col-sm-4 col-xs-12">
+                                                    <article class="entry-item">
+                                                        <div class="entry-thumb"> <a href="https://drive.google.com/drive/folders/1_gNlznSxzDhzxBT2ENomZOV6KTLjaMfU" target="_blank"> <img typeof="foaf:Image" src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/beyond_the_kitchen-2025.jpg" width="398" height="266"> </a></div>
+                                                        <div class="entry-content">
+                                                            <header class="clearfix"> <span class="entry-time-white pull-left">19 Feb 2025</span> </header>
+                                                            <h3 class="entry-title happenings"><a href="https://drive.google.com/drive/folders/1_gNlznSxzDhzxBT2ENomZOV6KTLjaMfU" target="_blank">Beyond The Kitchen - 2025</a></h3>
+                                                            <div class="qna-2">
+                                                                <div class="question-2"><span></span></div>
+                                                                <div class="answer-2">
+                                                                    <p>The Entrepreneurship Club of UCSI University Bangladesh Branch Campus organised “Beyond the Kitchen: Business Lessons from a Chefpreneur”, featuring Executive Chef Kazi Piash from Pearl Hotel.</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                </div>
+                                            </span>
+                                        </div>
+                                        <div class="views-field views-field-title"> <span class="field-content">
+                                                <div class="col-md-4 col-sm-4 col-xs-12">
+                                                    <article class="entry-item">
+                                                        <div class="entry-thumb"> <a href="https://drive.google.com/drive/folders/12T2pvD5iWv_vebpypC58FFctfPN7VrzM" target="_blank"> <img typeof="foaf:Image" src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/tarunner_utshob-2025.jpg" width="398" height="266"> </a></div>
+                                                        <div class="entry-content">
+                                                            <header class="clearfix"> <span class="entry-time-white pull-left">16 Feb 2025</span> </header>
+                                                            <h3 class="entry-title happenings"><a href="https://drive.google.com/drive/folders/12T2pvD5iWv_vebpypC58FFctfPN7VrzM" target="_blank">Tarunner Utshob - 2025</a></h3>
+                                                            <div class="qna-2">
+                                                                <div class="question-2"><span></span></div>
+                                                                <div class="answer-2">
+                                                                    <p>Festival of Youth – A Celebration of Bengali, Culture & Sports was organized by the Cultural Club & Outdoor Sports Club.</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                </div>
+                                            </span>
+                                        </div>
+                                        <div class="views-field views-field-title"> <span class="field-content">
+                                                <div class="col-md-4 col-sm-4 col-xs-12">
+                                                    <article class="entry-item">
+                                                        <div class="entry-thumb"> <a href="https://ucsiuniversity073.sharepoint.com/sites/GCA.BD/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FGCA%2EBD%2FShared%20Documents%2FGCA%20BD%2FPhotos%2FStudent%20Affairs%20%26%20Alumni%2FMind%20Marathon%20%2D%20Kahoot&p=true&ga=1" target="_blank"> <img typeof="foaf:Image" src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/mind_marathon-2025.jpg" width="398" height="266" alt="" title=""> </a></div>
+                                                        <div class="entry-content">
+                                                            <header class="clearfix"> <span class="entry-time-white pull-left">12 Feb 2025</span> </header>
+                                                            <h3 class="entry-title happenings"><a href="https://ucsiuniversity073.sharepoint.com/sites/GCA.BD/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FGCA%2EBD%2FShared%20Documents%2FGCA%20BD%2FPhotos%2FStudent%20Affairs%20%26%20Alumni%2FMind%20Marathon%20%2D%20Kahoot&p=true&ga=1" target="_blank">Mind Marathon - 2025</a></h3>
+                                                            <div class="qna-2">
+                                                                <div class="question-2"><span></span></div>
+                                                                <div class="answer-2">
+                                                                    <p>The Student Affairs and Alumni (SAA) Department arranged "Mind Marathon", a fascinating event, with the exploration of Kahoot, a Norwegian online game-based learning platform.</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                </div>
+                                            </span>
+                                        </div>
+                                    </div> 
+                                </div>
+                                <script type="text/javascript">
+                                    let question = document.querySelectorAll(".question-2");
+                                    question.forEach((qsitem) => {
+                                        qsitem.addEventListener("click", function(e) {
+                                            //   store the .answer div containing the answer
+                                            let sibling = qsitem.nextElementSibling;
+                                            // Nested loop for removing active class from all and set answer height to 0
+                                            question.forEach((item) => {
+                                                item.nextElementSibling.style.height = "0px";
+                                                //   remove class "active" except for the currently clicked item
+                                                item != qsitem && item.parentNode.classList.remove("active");
+                                            });
+                                            //then toggle the "active" class of clicked item's parent ".qna"
+                                            this.parentNode.classList.toggle("active");
+                                            // set actual height for .answer div if .qna has the class "active"
+                                            if (qsitem.parentNode.classList.contains("active")) {
+                                                sibling.style.height = sibling.scrollHeight + "px";
+                                            } else {
+                                                sibling.style.height = "0px";
+                                            }
+                                        });
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

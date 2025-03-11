fac4118157b7485f502ebcc7a6956680b3718766
Fahim6989 <fahimfoisoleee@gmail.com>
2025-03-11 12:39:22 +0600

    header

--- top-header.php
+++ top-header.php
@@ -6,7 +6,7 @@
     <div class="widget kopa-social-link-widget">
         <ul class="social-nav model-2 clearfix">
             <li><a href="https://www.facebook.com/ucsiuniversitybangladeshbranchcampus" class="facebook"><i class="fab fa-facebook"></i></a></li>
-            <li><a href="https://api.whatsapp.com/send?phone=8801844616516" class="whatsapp"><i class="fab fa-whatsapp"></i></a></li>
+            <li><a href="https://api.whatsapp.com/send?phone=8801949943444" class="whatsapp"><i class="fab fa-whatsapp"></i></a></li>
         </ul>
     </div>
 </div>

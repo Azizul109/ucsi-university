48615951471b55d3a444b131d129ec899b35f0bc
Fahim6989 <fahimfoisoleee@gmail.com>
2025-03-19 13:18:28 +0600

    news

--- featured-in-news.php
+++ featured-in-news.php
@@ -8,6 +8,14 @@
         </div>
         <div class="widget-content">
             <ul class="table-list">
+            <div class="views-field views-field-title"> <span class="field-content">
+                        <li class="clearfix">
+                            <div class="Mdate-col">18-Mar-2025</div>
+                            <div class="media-col">The Daily Campus</div>
+                            <div class="title-col"><a href="https://thedailycampus.com/private-university/175329" target="_blank">ইউসিএসআই ইউনিভার্সিটির সোশ্যাল সায়েন্স ও আর্টস ফ্যাকাল্টি কেন সেরা, কেন পড়বেন</a></div>
+                        </li>
+                    </span>
+                </div>
                 <div class="views-field views-field-title"> <span class="field-content">
                         <li class="clearfix">
                             <div class="Mdate-col">17-Mar-2025</div>

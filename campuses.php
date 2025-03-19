074df45a178689fc90b6c149f53770ee21c0f63f
azizularif420 <arif@mazegeek.com>
2025-03-19 22:54:16 +0600

    bangladesh branch campus page

---
+++ campuses.php
@@ -0,0 +1,104 @@
+<script>
+    function myPopup(myURL, title, myWidth, myHeight) {
+        var left = (screen.width - myWidth) / 2;
+        var top = (screen.height - myHeight) / 4;
+        var myWindow = window.open(myURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + myWidth + ', height=' + myHeight + ', top=' + top + ', left=' + left);
+    }
+</script>
+
+<section class="kopa-area kopa-area-parallax" style="background-color:#f7f7f7">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+                <div class="widget kopa-owl-5-widget">
+                    <div class="widget-title widget-title-s5 text-center">
+                        <!--<div class="element-title text-center">-->
+                        <span></span>
+                        <h2>Campuses</h2>
+                    </div>
+                    <!-- element-title -->
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-6 col-md-3 text-center mb-10">
+                        <div class="campus-box">
+                            <a href="https://www.ucsiuniversity.edu.my/kuala-lumpur-campus" target="_blank"><img
+                                    src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/kl_campus_thumb_3.jpg"
+                                    alt="Kuala Lumpur Campus" /></a>
+                            <h5 style="color: #48535e; font-size:18px;font-weight:700;"><a
+                                    href="https://www.ucsiuniversity.edu.my/kuala-lumpur-campus" target="_blank">Kuala
+                                    Lumpur<br>Campus</a></h5>
+                            <!-- <p style="font-size:13px;margin-top: -12px;">DU020(W)</p> -->
+
+                            <p class="pt-10" style="font-size:16px;"><a
+                                    href="https://www.ucsiuniversity.edu.my/kuala-lumpur-campus" target="_blank"><i
+                                        class="fa fa-angle-right"></i> <b>Find out more</b></a></p>
+                            <!-- <p class="pt-10"><a class="kopa-button red-button small-button" onclick="myPopup ('https://wa.me/60391018882', 'web', 800, 500);">Chat with Counsellor</a></p> -->
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 text-center mb-10">
+                        <div class="campus-box">
+                            <a href="https://www.ucsiuniversity.edu.my/kuching-campus" target="_blank"><img
+                                    src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/sarawak_campus.jpg"
+                                    alt="Kuching Campus" /></a>
+                            <h5 style="color: #48535e; font-size:18px;font-weight:700;"><a
+                                    href="https://www.ucsiuniversity.edu.my/kuching-campus"
+                                    target="_blank">Kuching<br>Campus</a></h5>
+                            <!-- <p style="font-size:13px;margin-top: -12px;">DU020-02(Q)</p> -->
+                            <p class="pt-10" style="font-size:16px;"><a
+                                    href="https://www.ucsiuniversity.edu.my/kuching-campus" target="_blank"><i
+                                        class="fa fa-angle-right"></i> <b>Find out more</b></a></p>
+                            <!-- <p class="pt-10"><a class="kopa-button red-button small-button" onclick="myPopup ('https://wa.me/60391018882', 'web', 800, 500);">Chat with Counsellor</a></p> -->
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 text-center mb-10">
+                        <div class="campus-box">
+                            <a href="https://www.ucsiuniversity.edu.my/springhill-campus" target="_blank"><img
+                                    src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/springhill-campus.jpg"
+                                    alt="Springhill (Seremban/PD) Campus" /></a>
+                            <h5 style="color: #48535e; font-size:18px;font-weight:700;letter-spacing: 0px;"><a
+                                    href="https://www.ucsiuniversity.edu.my/springhill-campus"
+                                    target="_blank">Springhill (Seremban/PD) Campus</a></h5>
+                            <!-- <p style="font-size:13px;margin-top: -12px;">DU020(W)</p> -->
+                            <p class="pt-10" style="font-size:16px;"><a
+                                    href="https://www.ucsiuniversity.edu.my/springhill-campus" target="_blank"><i
+                                        class="fa fa-angle-right"></i> <b>Find out more</b></a></p>
+                            <!-- <p class="pt-10"><a class="kopa-button red-button small-button" onclick="myPopup ('https://wa.me/60391018882', 'web', 800, 500);">Chat with Counsellor</a></p> -->
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 text-center mb-10">
+                        <div class="campus-box">
+                            <a href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh-branch-campus"> <img
+                                    src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/bangadesh_campus_2.jpg"
+                                    alt="Bangladesh Campus" /></a>
+                            <h5 style="color: #48535e;font-size:18px;font-weight:700;"><a
+                                    href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh-branch-campus">Bangladesh
+                                    Branch<br> Campus</a></h5>
+                            <!-- <p style="color: #48535e;font-size: 13px;line-height: 18px;font-style: italic;margin: -10px 10px 0;">(Approved by the Ministry of Education, People’s Republic of Bangladesh)</p> -->
+                            <p class="pt-10" style="font-size:16px;"><a
+                                    href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh-branch-campus"><i
+                                        class="fa fa-angle-right"></i> <b>Find out more</b></a></p>
+                            <!-- <p class="pt-10"><a class="kopa-button red-button small-button" href="https://apps.ucsiuniversity.edu.my/bangladesh-enquiry" target="_blank">ENQUIRE NOW</a></p> -->
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- col-md-12 -->
+
+        </div>
+        <!-- row -->
+
+    </div>
+    <!-- container -->
+
+</section>
+<!-- kopa-area-3 -->

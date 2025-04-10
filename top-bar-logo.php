1060538dc84aa0c708f828d838712833bddb0298
Fahim6989 <fahimfoisoleee@gmail.com>
2025-04-10 17:57:59 +0600

    credit transfer

---
+++ top-bar-logo.php
@@ -0,0 +1,13 @@
+<div class="main-logo" style="float:left;">
+    <span><a href="https://www.bangladesh.ucsiuniversity.edu.my/"><img src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/logo-new-2-bangladesh-ucsi.png" alt="UCSI University"></a></span>
+    <span><img src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/tag-130x60.png" alt="QS Ranking" class="QS_logo"></span>
+    <span><img src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/ugc_48x48.png" alt="UGC" class="UGC_logo"></span>
+</div>
+<div class="inside" style="float:right;">
+    <div class="widget kopa-social-link-widget">
+        <ul class="social-nav model-2 clearfix">
+            <li><a href="https://www.facebook.com/ucsiuniversitybangladeshbranchcampus" class="facebook"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="https://api.whatsapp.com/send?phone=8801949943444" class="whatsapp"><i class="fab fa-whatsapp"></i></a></li>
+        </ul>
+    </div>
+</div>

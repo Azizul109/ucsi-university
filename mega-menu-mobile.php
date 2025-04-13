4f63ac6dde61c489dce2b8aeb6ae9ad1c1f7852b
Fahim6989 <fahimfoisoleee@gmail.com>
2025-04-13 17:34:53 +0600

    mobile menu

--- mega-menu-mobile.php
+++ mega-menu-mobile.php
@@ -6,10 +6,22 @@
                     <ul class="main-menu-mobile clearfix" style="display: block;">
                         <li><a href="#">About Us<span class=""></span></a>
                             <ul class="menu" data-index="0" style="display: none;">
-                                <li><a href="#">The University</a></li>
+                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh/about-the-campus">The University<span class=""></span></a>
+                                    <ul class="menu" data-index="1" style="display: none;">
+                                        <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/bangladesh/about-the-campus">Overview</a></li>
+                                        <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/about-ucsi/affiliation-ranking-recognition/ranking">Awards &amp; Recognition</a></li>
+                                    </ul>
+                                </li>
                                 <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/about-ucsi/the-founder?display=tu">The Founder</a></li>
                                 <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/staff-directory/bangladesh/board-of-directors-and-top-management?display=ul">Board of Directors</a></li>
-                                <li><a href="#">University Leadership</a></li>
+                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/the-pro-chancellor?display=ul">University Leadership<span class=""></span></a>
+                                    <ul class="menu" data-index="2" style="display: none;">
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/the-pro-chancellor?display=ul"> The Pro-Chancellor </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/the-vice-chancellor?display=ul"> The Vice-Chancellor </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/university-council"> University Council </a></li>
+                                        <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/senate-members"> Senate Members </a></li>
+                                    </ul>
+                                </li>
                                 <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/academic-advisory-board">Board of Academic Advisors</a></li>
                                 <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/deputy-vice-chancellor">Deputy Vice-Chancellor</a></li>
                                 <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/provost">Provost</a></li>
@@ -18,44 +30,58 @@
                             </ul>
                         </li>
                         <li class=""><a href="#">Academic<span class=""></span></a>
-                            <ul class="menu" data-index="1" style="display: none;">
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/programmes/bangladesh">Programmes</a></li>
-                                <li><a href="#">Faculties</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Admissions<span class=""></span></a>
-                            <ul class="menu" data-index="2" style="display: none;">
-                                <li><a href="#">Bangladeshi Students</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/intakes-and-calendar">Intakes and Calendar</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/exhibitions-and-roadshows">Exhibitions And Roadshows</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/scholarships">Scholarships</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/Tuition-Fees">Tuition Fees</a></li>
-                                <li><a href="https://bangladesh.ucsiuniversity.edu.my/downloads">Downloads</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Life at UCSI<span class=""></span></a>
                             <ul class="menu" data-index="3" style="display: none;">
-                                <li><a href="#">Library Services</a></li>
-                                <li><a href="#">Student Experience</a></li>
-                            </ul>
+                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/programmes/bangladesh">Programmes</a></li>
                         </li>
-                        <li class=""><a href="#">Facilities<span class=""></span></a>
-                            <ul class="menu" data-index="4" style="display: none;">
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/all-campus-facilities">Campus Facilities</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/student-counsiling">Student Counselling and Enrollment Center</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/security-services">Security Services</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/lobby">Spacious Lobby</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Publicity<span class=""></span></a>
-                            <ul class="menu" data-index="5" style="display: none;">
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/featured-in-news/all/all">Press &amp; Publicity</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/photo-gallery/all/all">Photo Gallery</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/video-gallery">Video Gallery</a></li>
-                                <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/announcement">Notices &amp; Announcements</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="https://www.ucsiuniversity.edu.my/">UCSI University</a></li>
+                    </ul>
+                    </li>
+                    <li class=""><a href="#">Faculties<span class=""></span></a>
+                        <ul class="menu" data-index="5" style="display: none;">
+                            <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/school-of-graduate-studies"> School of Graduate Studies </a></li>
+                            <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-business-and-management"> Faculty of Business And Management </a></li>
+                            <!-- <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-engineering"> Faculty of Engineering </a></li> -->
+                            <!-- <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-hospitality-and-tourism-management"> Faculty of Hospitality &amp; Tourism Management </a></li> -->
+                            <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-social-sciences-and-liberal-arts"> Faculty of Social Sciences &amp; Liberal Arts </a></li>
+                            <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-creative-arts-design-and-architecture"> Faculty of Creative Arts And Design </a></li>
+                            <li> <a href="https://www.bangladesh.ucsiuniversity.edu.my/about-us/faculty-of-computer-science-and-digital-innovation"> Faculty of Computer Science And Digital Innovation </a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Admissions<span class=""></span></a>
+                        <ul class="menu" data-index="6" style="display: none;">
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/admission/bangladeshi-students/overview">Bangladeshi Students</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/intakes-and-calendar">Intakes and Calendar</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/exhibitions-and-roadshows">Exhibitions And Roadshows</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/scholarships">Scholarships</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/Tuition-Fees">Tuition Fees</a></li>
+                            <li><a href="https://bangladesh.ucsiuniversity.edu.my/downloads">Downloads</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Life at UCSI<span class=""></span></a>
+                        <ul class="menu" data-index="7" style="display: none;">
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/library-0">Library Services</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/student-affairs-and-alumni">Student Experience</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/graduate-attribute">Graduate Attribute</a></li>
+                        </ul>
+                    </li>
+                    <li class=""><a href="#">Facilities<span class=""></span></a>
+                        <ul class="menu" data-index="8" style="display: none;">
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/all-campus-facilities">Campus Facilities</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/student-counsiling">Student Counselling and Enrollment Center</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/credit-transfer-facilities/study-in-united-kingdom">Credit Transfer Facilities</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/security-services">Security Services</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/facilities/lobby">Spacious Lobby</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Publicity<span class=""></span></a>
+                        <ul class="menu" data-index="9" style="display: none;">
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/featured-in-news/all/all">Press &amp; Publicity</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/photo-gallery/all/all">Photo Gallery</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/video-gallery">Video Gallery</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/ucsi-bangladesh-horizon">UCSI Horizon</a></li>
+                            <li><a href="https://www.bangladesh.ucsiuniversity.edu.my/announcement">Notices &amp; Announcements</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="https://www.ucsiuniversity.edu.my/">UCSI University</a></li>
                     </ul>
                 </nav>
             </div>

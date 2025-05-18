28ffda32f845ad4c9eab532ee8d76109e462dbb9
Azizul109 <azizularifnahim52@gmail.com>
2025-05-18 17:52:07 +0600

    ..

--- tuition-fees.php
+++ tuition-fees.php
@@ -13,7 +13,7 @@
 
     <div class="pdf-embed" style="width: 100%; height: 500px;">
         <h4 style="text-align: center" class="mb-1 text-4 font-weight-bold">Undergraduate</h4>
-        <iframe src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/course_breakdown_flyer_new.pdf" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://www.bangladesh.ucsiuniversity.edu.my/sites/default/files/course_breakdown_flyer_2025.pdf" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
     </div>
 </div>
 
